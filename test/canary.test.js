508e7d92ddd4a5f7d6193b3b622091786463bea6
Travo100 <Travo100@gmail.com>
2019-01-09 10:35:44 -0800

    Added some unit tests

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,9 +1,29 @@
 var expect = require("chai").expect;
 
+function multiply(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new Error("x or y is not a number.");
+  } else {
+    return x * y;
+  }
+}
+
 describe("canary test", function() {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
   it("should pass this canary test", function() {
     expect(true).to.be.true;
+  });
+});
+
+describe("Multiply", function() {
+  it("should multiply properly two numbers", function() {
+    expect(multiply(2, 4)).to.equal(8);
+  });
+
+  it("should throw an error is not passed a number", function() {
+    expect(function() {
+      multiply(2, "8");
+    }).to.throw(Error);
   });
 });
